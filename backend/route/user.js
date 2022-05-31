304b00d79c9f958f78021d7e1c908c88a0d7d64c
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-05-31 10:35:09 +0200

    openid is in progress

--- backend/route/user.js
+++ backend/route/user.js
@@ -1,7 +1,56 @@
 const router = require("express").Router();
-const { login, user } = require("../controller/login");
+const User = require("../model/user");
+const { user } = require("../controller/login");
 
-router.post("/api/login", login);
+const config = {
+  google: {
+    clientId: "",
+    clientSecret: "",
+    redirectUri: "",
+    tokenEndpoint: "",
+  },
+  // facebook: {
+  //   clientId: "",
+  //   clientSecret: "",
+  //   redirectUri: "",
+  //   tokenEndpoint: "",
+  // },
+  // github: {
+  //   clientId: "",
+  //   clientSecret: "",
+  //   redirectUri: "",
+  //   tokenEndpoint: "",
+  // },
+};
+
+router.post("/api/login", async (req, res) => {
+  const payload = req.body;
+  if (!payload) return res.status(400).send("Nice try");
+
+  const code = payload.code;
+  const provider = payload.provider;
+  if (!code || !provider) return res.status(400).send("Nice try");
+  if (Object.keys(config).includes("provider")) return res.status(400).send("Nice try");
+
+  const link = configProvider.tokenEndpoint;
+  const configProvider = config[provider];
+
+  let response;
+  try {
+    response = await http.post(link, {
+      code: code,
+      client_id: configProvider.clientId,
+      client_secret: configProvider.clientSecret,
+      redirect_uri: configProvider.redirectUri,
+      grant_type: "authorization_code",
+    });
+  } catch (err) {
+    return res.status(401).send();
+  }
+
+  //
+});
+
 /* tutorial */
 router.post("/", user);
 /* tutorial */
