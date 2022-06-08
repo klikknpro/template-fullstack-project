549b8cdde61275e8df52dfa665a0215a3c803f00
klikknpro <80091502+klikknpro@users.noreply.github.com>
2022-06-08 10:58:11 +0200

    add morgan

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const logger = require("./middleware/logger");
+// const logger = require("./middleware/logger");
+const morgan = require("morgan");
 const errorHandler = require("./middleware/errorHandler");
 
 const corsOptions = {
@@ -11,7 +12,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.use(express.json()); // body-ban erkezo json-t parse-olni tudja
-app.use([logger]); // use this middleware on every request
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms")); // use this middleware on every request
 
 const dashboardRoutes = require("./route/dashboard");
 app.use("/api/dashboards", dashboardRoutes);
